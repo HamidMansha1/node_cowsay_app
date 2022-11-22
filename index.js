f8556c5809d95761b401ab10f13b1baa50183399
HamidMansha1 <hamidmansha115@gmail.com>
2022-11-22 10:18:31 -0800

    multiple  environment variable values

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var cowsay = require("cowsay");
 require('dotenv').config();
 
 console.log(cowsay.say({
-    text : "I'm a moooodule",
-    e : "oO",
-    T : "U "
+    text : "I'm a Hey Kashif",
+    e : "y",
+    T : "Kashif "
 }));
 
 // or cowsay.think()
 
 
-console.log(`friend_name : ${process.env.friend_name}`)
+// console.log(`friend_name : ${process.env.friend_name}`)
